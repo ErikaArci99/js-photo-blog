5274a46e6d8d831dd4887ff73f31ed1224da7a76
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-07 16:16:10 +0200

    [UPD] finito milestone 1 CON BONUS

--- js/script.js
+++ js/script.js
@@ -5,12 +5,27 @@ const div = document.getElementById('div');
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
 // funzione che mi crea la card
-const createImgCard = (endpoint) =>{
-    const card = `<div class="card-img bg-white d-flex flex-column align-items-start p-3 position-relative">
-                        <img src="${endpoint.url}" alt="Immagine" class="pin">
-                        <div class="card-content bg-danger-subtle"></div>
-                        <p class="sometype-mono text-start mb-1 mt-2 text-secondary">${endpoint.date}</p>
-                        <h5 class="sometype-mono text-start mb-0 fs-3 fw-bolder">${endpoint.title}</h5>
-                    </div>`;
-    div.innerHTML += card;
-}
+const createImgCard = (data) => {
+    const card = `
+        <div class="col-12 col-md-6 col-lg-4 mb-5">
+            <div class="card-content bg-white d-flex flex-column align-items-start p-3 position-relative">
+                <img src="./img/pin.svg" alt="Immagine" class="pin">
+                <img src="${data.url}" alt="Immagine" class="card-img">
+                <p class="sometype-mono text-start mb-1 mt-2 text-secondary">${data.date}</p>
+                <h5 class="sometype-mono text-start mb-0 fs-3 fw-bolder">${data.title}</h5>
+            </div>
+        </div>
+    `;
+    div.innerHTML += card; 
+};
+
+// recupero dati 
+const recuperoDati = () => {
+    axios.get(endpoint)
+        .then(response => {
+            response.data.forEach(item => createImgCard(item)); 
+        });
+};
+
+// chiamo la funzione
+recuperoDati();
