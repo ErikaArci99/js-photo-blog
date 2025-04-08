9a3f190dee4f50d6cabdd2bacc14998dadb1baf7
ErikaArci99 <erikaarci1999@outlook.it>
2025-04-08 13:08:43 +0200

    [UPD] aggiunto bonus hover e aggiunta img overlay

--- js/script.js
+++ js/script.js
@@ -3,9 +3,7 @@ const div = document.getElementById('div');
 const containerOverlay = document.getElementById('container-overlay');
 const imgOverlay = document.getElementById('img-overlay');
 const closeButton = document.getElementById('button');
-
-// dichiaro variabili
-const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 // funzione che mi crea la card
 const createImgCard = (data) => {
@@ -13,22 +11,35 @@ const createImgCard = (data) => {
         <div class="col-12 col-md-6 col-lg-4 mb-5">
             <div class="card-content bg-white d-flex flex-column align-items-start p-3 position-relative">
                 <img src="./img/pin.svg" alt="Immagine" class="pin">
-                <img src="${data.url}" alt="Immagine" class="card-img">
+                <img src="${data.url}" alt="Immagine" class="card-img" data-url="${data.url}">
                 <p class="sometype-mono text-start mb-1 mt-2 text-secondary">${data.date}</p>
                 <h5 class="sometype-mono text-start mb-0 fs-3 fw-bolder">${data.title}</h5>
             </div>
         </div>
     `;
-    div.innerHTML += card; 
+    div.innerHTML += card;
 };
 
 // recupero dati 
 const recuperoDati = () => {
     axios.get(endpoint)
         .then(response => {
-            response.data.forEach(item => createImgCard(item)); 
+            response.data.forEach(item => createImgCard(item));
+
+            // aggiungiamo gli event listener alle immagini dopo averle caricate
+            const cardImages = document.querySelectorAll('.card-img');
+            cardImages.forEach(img => {
+                img.addEventListener('click', function () {
+                    imgOverlay.src = this.getAttribute('data-url');
+                    closeButton.classList.remove('d-none');
+                    containerOverlay.classList.remove('d-none');
+                    imgOverlay.classList.remove('d-none');
+                });
+            });
         });
 };
 
 // chiamo la funzione
 recuperoDati();
+
+
